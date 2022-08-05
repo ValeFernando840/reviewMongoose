c9a91a3c9c61b849b1d8a323cf6fb7e4d16dd348
Alexis <vale.alexis18@gmail.com>
2022-08-05 12:01:27 -0300

    agregado de Validator(libreria de validaciones), mensajes de error personalizado, y Virtuals

--- course.js
+++ course.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator').isEmail; 
 
 //1. Define Schema
 const courseSchema = new mongoose.Schema({
@@ -6,18 +7,13 @@ const courseSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true, 
-    validate:{
-      validator: function(value){
-        return true
-      },
-      message: 'invalidate input'
-    }
-  },
+    validate: isEmail, // in this case is not necessary -.-
   description: {
     type: String,
-    minlength: 10,
+    minlength: [10,"doesn't meet minimun length"],
     maxlength: 300
 
+    }
   },
   numberOfTopics: {
     type: Number,
@@ -28,12 +24,20 @@ const courseSchema = new mongoose.Schema({
   publishedAd: {
     type: Date
   },
- 
 })
+courseSchema.virtual('info')
+  .get(function(){
+    //this => document
+    return `${this.description}. Topics: ${this.numberOfTopics}. release date:${this.publishedAt}`
+  })
+  .set(function(value){
+    
+  });
 //2. Define Model
 mongoose.model('Course', courseSchema);
 
 /**
  * Note: 
  * you can use match: /regular expression/ to validate that input
+ * you can import form validator and then in validate: isEmail
  */
